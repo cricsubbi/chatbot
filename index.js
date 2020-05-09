868ccf83509feb508a5e31fbcbee6194c3c45144
unknown <imsubodhrane@gmail.com>
2020-05-09 20:26:17 +0530

    4th

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require("body-parser") 
 const allInfo = require("./api")
 const app = express()
-const port = process.env.PORT
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set("view engine","ejs");
 app.use(express.static("views"));
